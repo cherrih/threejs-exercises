c3494ac22a377697dee7fabcb85e1371fdc67c69
cherrih <cherri.hartigan@gmail.com>
2019-05-22 16:08:07 -0700

    Add sphere to light

--- 01_02/main.js
+++ 01_02/main.js
@@ -9,6 +9,8 @@ function init() {
 
   var box = getBox(1,1,1);
   var plane = getPlane(20);
+  var pointLight = getPointLight(1);
+  var sphere = getSphere(0.05);
 
   plane.name = 'plane-1';
   box.name = 'box-1';
@@ -16,11 +18,14 @@ function init() {
   // radians not angles 
   plane.rotation.x = Math.PI / 2;
   box.position.y = box.geometry.parameters.height/2;
+  pointLight.position.y = 1.23;
 
   // plane.position.y = 1;
 
   scene.add(box);
   scene.add(plane);
+  pointLight.add(sphere);
+  scene.add(pointLight);
 
   var camera = new THREE.PerspectiveCamera(
     45, //fov
@@ -37,7 +42,7 @@ function init() {
   var renderer = new THREE.WebGLRenderer();
   // set size of renderer
   renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setClearColor('#fff');
+  renderer.setClearColor('rgb(120,120,120)');
   document.getElementById('webgl').appendChild(renderer.domElement);
   update(renderer, scene, camera);
   return scene;
@@ -45,8 +50,8 @@ function init() {
 
 function getBox(w,h,d) {
   var geometry = new THREE.BoxGeometry(w,h,d);
-  var material = new THREE.MeshBasicMaterial({
-    color: 0x00fff0
+  var material = new THREE.MeshPhongMaterial({
+    color: 'rgb(120,120,120)'
   });
   return new THREE.Mesh(
     geometry,
@@ -56,14 +61,29 @@ function getBox(w,h,d) {
 
 function getPlane(size) {
   var geometry = new THREE.PlaneGeometry(size,size);
-  var material = new THREE.MeshBasicMaterial({
-    color: 0xff0000,
+  var material = new THREE.MeshPhongMaterial({
+    color: 'rgb(120,120,120)',
     side: THREE.DoubleSide
   });
   return new THREE.Mesh(
     geometry,
     material
   );
+}
+
+function getSphere(radius) {
+  var geometry = new THREE.SphereGeometry(radius, 24, 24);
+  var material = new THREE.MeshBasicMaterial({
+    color: 'rgb(255, 255, 255)'
+  });
+  return new THREE.Mesh(
+    geometry,
+    material
+  );
+}
+
+function getPointLight(intensity) {
+  return new THREE.PointLight(0xffffff, intensity);
 }
 
 function update(renderer, scene, camera) {
