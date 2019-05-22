b363d017787d9589f0a6cde5f9a68853f74ea369
cherrih <cherri.hartigan@gmail.com>
2019-05-22 11:19:57 -0700

    Set up scene

--- 01_02/main.js
+++ 01_02/main.js
@@ -1,1 +1,19 @@
-console.log(THREE);
+
+function init() {
+  var scene = new THREE.Scene();
+  var camera = new THREE.PerspectiveCamera(
+    45, //fov
+    window.innerWidth/window.innerHeight, //aspect ratio
+    1, // near
+    1000 //far clipping plane
+  ); 
+  var renderer = new THREE.WebGLRenderer();
+  // set size of renderer
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.getElementById('webgl').appendChild(renderer.domElement);
+  renderer.render(
+    scene,
+    camera
+  );
+}
+init();
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # threejs-exercises
+
+#### Scene object
+Container for every other 3D object. Represents the world you will be building.
+
+#### Camera
+Eyes we will be seeing the 3D world through. Requires several options.
+- FOV
+- Aspect ratio
+- near and far clipping planes (anything beyond these distances won't be seen)
+
+#### Renderer
+Converts 3D data to 2D image. We will use WebGL renderer. Can use others eg canvas or SVG, but end user experience not as great
